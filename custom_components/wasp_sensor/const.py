7923c9f1f61db5d546b338359688254635e14af7
fourtrax01 <40325979+fourtrax01@users.noreply.github.com>
2025-09-27 12:35:14 -0700

    Update const.py
    Just updating to fix issues with HA changes

--- custom_components/wasp_sensor/const.py
+++ custom_components/wasp_sensor/const.py
@@ -1,13 +1,10 @@
-"""Constants for integration_blueprint."""
-# Base component constants
+"""Constants for Wasp Sensor."""
+
 NAME = "Wasp Sensor"
 DOMAIN = "wasp_sensor"
 DOMAIN_DATA = f"{DOMAIN}_data"
-VERSION = "0.0.1"
-ISSUE_URL = "http://google.com"
-
-# Device classes
-BINARY_SENSOR_DEVICE_CLASS = "occupancy"
+VERSION = "0.0.4"
+ISSUE_URL = "https://github.com/dlashua/hass-wasp_sensor/issues"
 
 # Platforms
 BINARY_SENSOR = "binary_sensor"
@@ -19,7 +16,7 @@ SERVICE_RELOAD = "reload"
 # Defaults
 DEFAULT_NAME = DOMAIN
 
-# Config Elements
+# Config keys
 CONF_WASP_SENSORS = "wasp_sensors"
 CONF_WASP_INV_SENSORS = "wasp_inv_sensors"
 CONF_BOX_SENSORS = "box_sensors"
@@ -27,10 +24,9 @@ CONF_BOX_INV_SENSORS = "box_inv_sensors"
 CONF_TIMEOUT = "timeout"
 CONF_NAME = "name"
 
-# Configuration
+# Behavior
 SENSOR_CHANGE_DELAY = 1
 DEFAULT_WASP_TIMEOUT = 5
-
 
 STARTUP_MESSAGE = f"""
 -------------------------------------------------------------------
